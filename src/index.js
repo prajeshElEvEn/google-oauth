4656e12c81672cd910e24472e37e678514b6cbd2
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-07-23 04:43:12 +0530

    facing info is undefined error

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ function handleSignIn() {
     form.setAttribute('action', oauthEndpoint)
 
     let params = {
-        "clientId": "157489452148-1mt69i2jsmhc5sertanfu25jttc6mvhc.apps.googleusercontent.com",
-        "redirectUri": "http://127.0.0.1:5500/pages/profile.html",
-        "responseType": "token",
+        "client_id": "157489452148-1mt69i2jsmhc5sertanfu25jttc6mvhc.apps.googleusercontent.com",
+        "redirect_uri": "http://127.0.0.1:5500/pages/profile.html",
+        "response_type": "token",
         "scope": "https://www.googleapis.com/auth/userinfo.profile",
-        "includeGrantedScopes": "true",
+        "include_granted_scopes": "true",
         "state": "pass-through-value"
     }
+
+    for (var param in params) {
+        let input = document.createElement('input')
+        input.setAttribute('type', 'hidden')
+        input.setAttribute('name', param)
+        input.setAttribute('value', params[param])
+        form.appendChild(input)
+    }
+
+    document.body.appendChild(form)
+
+    form.submit()
 }
